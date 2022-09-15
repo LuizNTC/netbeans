5abad83e0cb1def700ed1f7b89d8ae756a9d7b1f
Luiz Gustavo Feix Machado <m120311@ACAD.UNISC.BR>
2022-09-15 20:16:38 -0300

    intial commit

---
+++ src/main/java/com/mycompany/githubteste/Githubteste.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.githubteste;
+
+/**
+ *
+ * @author m120311
+ */
+public class Githubteste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
